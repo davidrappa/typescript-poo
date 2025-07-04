d0f91a4f20a8ef046c560838409af8499e44ce65
David <davidrappa@outlook.com>
2025-07-04 11:08:29 -0300

    feat: add person functional files

---
+++ banking/Person-functional.ts
@@ -0,0 +1,26 @@
+export type Person = {
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+};
+
+export function getFullName(person: Person): string {
+  return `${person.firstName} ${person.lastName}`;
+}
+
+export function getAge(person: Person): number {
+  const today = new Date();
+  const age = today.getFullYear() - person.birthDate.getFullYear();
+  const isBirthdayPassed =
+    today.getMonth() > person.birthDate.getMonth() ||
+    (today.getMonth() === person.birthDate.getMonth() &&
+      today.getDate() >= person.birthDate.getDate());
+  return isBirthdayPassed ? age : age - 1;
+}
+
+export function updateFirstName(person: Person, firstName: string): Person {
+  return {
+    ...person,
+    firstName,
+  };
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,24 @@
+import {
+  getAge,
+  getFullName,
+  Person,
+  updateFirstName,
+} from "./banking/Person-functional";
+
 function main() {
-  console.log("Hello, World");
+  const maria: Person = {
+    firstName: "Maria",
+    lastName: "da Silva",
+    birthDate: new Date("1999-01-15"),
+  };
+
+  console.log(getFullName(maria));
+
+  console.log(getAge(maria));
+
+  const updatedMaria = updateFirstName(maria, "Maria Clara");
+
+  console.log(updatedMaria);
 }
 
 main();
