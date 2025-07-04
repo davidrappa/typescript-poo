b56094782216a0587071cced74587690c91023ce
David <davidrappa@outlook.com>
2025-07-04 11:24:56 -0300

    feat: update person class with new methods

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import { Person } from "./banking/Person";
 function main() {
   const maria = new Person("Maria", "Silva", new Date("1996-01-15"));
 
-  console.log(maria);
+  maria.updateFirstName("Maria Clara");
+
+  console.log(maria.getFullName());
 }
 
 main();
