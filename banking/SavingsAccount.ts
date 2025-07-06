b5e755a202ef65b57d2e962edcd3283cd7207ccc
David <davidrappa@outlook.com>
2025-07-06 15:11:13 -0300

    feat: implement BankAccount, CurrentAccount, and SavingsAccount classes with basic functionalities

---
+++ banking/SavingsAccount.ts
@@ -0,0 +1,22 @@
+import { BankAccount } from "./BankAccount";
+import { Person } from "./Person";
+
+// subclass
+export class SavingsAccount extends BankAccount {
+  private interestRate: number; // taxa de juros
+  constructor(
+    person: Person,
+    initialBalance: number,
+    accountNumber: string,
+    interestRate: number
+  ) {
+    super(person, initialBalance, accountNumber);
+    this.interestRate = interestRate;
+  }
+
+  addInterest(): void {
+    const interest = this.balance * (this.interestRate / 100);
+    this.deposit(interest);
+    console.log(`Interest added: $${interest}. New Balance: $${this.balance}`);
+  }
+}
--- index.ts
+++ index.ts
@@ -1,15 +1,39 @@
+import { BankAccount } from "./banking/BankAccount";
+import { CurrentAccount } from "./banking/CurrentAccount";
 import { Person } from "./banking/Person";
+import { SavingsAccount } from "./banking/SavingsAccount";
 
-function main() {
-  const maria = new Person("Maria", "da Silva", new Date("1999-01-15"));
+const maria = new Person("Maria", "da Silva", new Date("1999-01-15"));
+const mariaSavingsAccount = new SavingsAccount(maria, 100, "123", 2);
 
-  //maria.firstName
+// mariaSavingsAccount.addInterest();
 
-  // maria.firstName = "Maria Clara"
-  maria.firstName = "";
+const david = new Person("David", "Rappa", new Date("1996-02-16"));
+const davidCurrentAccount = new CurrentAccount(david, 100, "1111", 100);
 
-  // console.log(maria.getFirstName());
-  console.log(maria.fullName);
-}
+// mariaSavingsAccount.withdraw(150);
 
-main();
+// davidCurrentAccount.withdraw(150);
+
+// /**
+//  *
+//  * @param sender envia
+//  * @param receiver recebe
+//  * @param amount quantidade
+//  */
+// function transaction(
+//   sender: BankAccount,
+//   receiver: BankAccount,
+//   amount: number
+// ) {
+//   if (amount > 0 && amount <= sender.balance) {
+//     sender.withdraw(amount);
+//     receiver.deposit(amount);
+//     console.log("Transaction successful");
+//   } else {
+//     console.log("Transaction failed: insufficient funds or invalid amount.");
+//   }
+// }
+
+// // enviar 50 do David para a Maria
+// transaction(davidCurrentAccount, mariaSavingsAccount, 50);
